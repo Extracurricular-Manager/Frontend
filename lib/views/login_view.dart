2fb108f9c8c5e8f0ae4a22a85ef063ba2b46a348
Arthur Lalande-Marchand <arthur.lalande-marchand@etudiant.univ-rennes1.fr>
2022-01-25 15:08:47 +0100

    add scaffold + keyboard

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -5,60 +5,63 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.fromLTRB(40, 100, 40, 100),
-      color: const Color(0xFF214A1F),
-      child: ListView(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), // radius of 10
-                color: Colors.white // green as background color
-            ),
-            child: Form(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    const Text("Connexion",
-                        style: TextStyle(
-                            height: 2, fontSize: 40, color: Color(0xFF214A1F))),
-                    const Text("\n"),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Nom d’utilisateur",
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: const EdgeInsets.fromLTRB(40, 100, 40, 100),
+        color: const Color(0xFF214A1F),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), // radius of 10
+                  color: Colors.white // green as background color
+              ),
+              child: Form(
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      const Text("Connexion",
+                          style: TextStyle(
+                              height: 2, fontSize: 40, color: Color(0xFF214A1F))),
+                      const Text("\n"),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Nom d’utilisateur",
+                        ),
+                        //controller: ,
                       ),
-                      //controller: ,
-                    ),
-                    const Text(""),
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Mot de passe",
+                      const Text(""),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: "Mot de passe",
+                        ),
+                        obscureText: true,
+                        //controller: ,
                       ),
-                      obscureText: true,
-                      //controller: ,
-                    ),
-                    const Text(""),
-                    Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10), // radius of 10
-                        color: const Color(0xFF214A1F),
+                      const Text(""),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10), // radius of 10
+                          color: const Color(0xFF214A1F),
+                        ),
+                        child: const TextButton(
+                          onPressed: null,
+                          child: Text('Connexion',
+                              style: TextStyle(color: Colors.white)),
+                        ),
                       ),
-                      child: const TextButton(
-                        onPressed: null,
-                        child: Text('Connexion',
-                            style: TextStyle(color: Colors.white)),
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
