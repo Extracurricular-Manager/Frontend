b37dd60b9c186ad82b21db9485a13bc0c26621b2
Curtis K <74824226+KLN05@users.noreply.github.com>
2022-02-02 12:07:22 +0100

    Update api_commons.dart

--- lib/data/api_abstraction/api_commons.dart
+++ lib/data/api_abstraction/api_commons.dart
@@ -16,15 +16,47 @@ class ApiCommons {
   String baseUrl = "/api";
 
   /// basic get operation (options currently passed through the endpoint parameter)
-  Future<T> getOperation<T>(BasicApiEndpoint sourceClass, String endpoint) async {
-    final generatedUrl = baseUrl+sourceClass.baseUrl+endpoint;
+  Future<T> getOperation<T>(
+      BasicApiEndpoint sourceClass, String endpoint) async {
+    final generatedUrl = baseUrl + sourceClass.baseUrl + endpoint;
     final response = await http.get(Uri.parse(generatedUrl));
-      if (response.statusCode == HttpStatus.ok) {
-        return sourceClass.createFromJson(jsonDecode(response.body));
-      } else {
-        throw Exception('Failed to load '+generatedUrl);
-      }
+    if (response.statusCode == HttpStatus.ok) {
+      return sourceClass.createFromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load ' + generatedUrl);
     }
   }
 
+  Future<T> postOperation<T>(
+      BasicApiEndpoint sourceClass, String endpoint) async {
+    final generatedUrl = baseUrl + sourceClass.baseUrl + endpoint;
+    final response = await http.post(Uri.parse(generatedUrl));
+    if (response.statusCode == HttpStatus.ok) {
+      return sourceClass.createFromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load ' + generatedUrl);
+    }
+  }
 
+  Future<T> putOperation<T>(
+      BasicApiEndpoint sourceClass, String endpoint) async {
+    final generatedUrl = baseUrl + sourceClass.baseUrl + endpoint;
+    final response = await http.put(Uri.parse(generatedUrl));
+    if (response.statusCode == HttpStatus.ok) {
+      return sourceClass.createFromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load ' + generatedUrl);
+    }
+  }
+
+  Future<T> patchOperation<T>(
+      BasicApiEndpoint sourceClass, String endpoint) async {
+    final generatedUrl = baseUrl + sourceClass.baseUrl + endpoint;
+    final response = await http.patch(Uri.parse(generatedUrl));
+    if (response.statusCode == HttpStatus.ok) {
+      return sourceClass.createFromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load ' + generatedUrl);
+    }
+  }
+}
