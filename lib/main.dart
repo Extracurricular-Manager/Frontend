21fa2dee46b96a15e26dfa9997b23b2332993284
Curtis K <74824226+KLN05@users.noreply.github.com>
2022-02-03 18:11:18 +0100

    workmanager background

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:dart_ping_ios/dart_ping_ios.dart';
 import 'package:flutter/material.dart';
+import 'package:frontendmobile/data/api_abstraction/api_commons.dart';
 import 'package:frontendmobile/routes.dart';
 import 'package:frontendmobile/test/test_view.dart';
 import 'package:logger/logger.dart';
+import 'package:workmanager/workmanager.dart';
 
 import 'data/api_abstraction/storage_utils.dart';
 
@@ -10,17 +12,36 @@ void main() {
   //Logger().v("Booting app...");
   DartPingIOS.register();
   runApp(const MyApp());
+
+  Workmanager().initialize(
+      callbackDispatcher, // The top level function, aka callbackDispatcher
+      isInDebugMode:
+          true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
+      );
+  Workmanager().registerPeriodicTask("1", "Background Push",
+      frequency: const Duration(minutes: 15));
+}
+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) {
+    return ApiCommons.SendToBack();
+  });
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({Key? key}) : super(key: key);
   static var log = Logger();
   @override
   Widget build(BuildContext context) {
-    StorageUtils().getVault("michel").then((value) => value.put("hello", "there"));
-    StorageUtils().getVault("michel").then((value) => value.put("bonjour", "ici"));
-    StorageUtils().getVault("michel").then((value) => value.size.then((value) => print(value)));
+    StorageUtils()
+        .getVault("michel")
+        .then((value) => value.put("hello", "there"));
+    StorageUtils()
+        .getVault("michel")
+        .then((value) => value.put("bonjour", "ici"));
+    StorageUtils()
+        .getVault("michel")
+        .then((value) => value.size.then((value) => print(value)));
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
